d48d9eaeabd6dc418f1b3af38da0f5aa67de0887
Jailson Eduardo Ferens <jailsonwin@gmail.com>
2023-05-10 16:11:40 -0300

    Delete stripe.ts

--- src/services/stripe.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  publicKey:
-    "pk_live_51N5vn5HTvInZMjOgkMFKPpckvTbeWibzPPu5zSdCCwBegU1oGYLhIxJ8xXOGAYjD5u7NAIM7qz0XsdSKPYpphBuF00TaNCR9OS",
-  secretKey:
-    "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",
-};
